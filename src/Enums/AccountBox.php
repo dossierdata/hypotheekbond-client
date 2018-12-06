5653d19b42db31ad68a943fdd4010af0925d4c12
Victor <victor.preda10@gmail.com>
2018-12-06 14:28:31 +0100

    Create new AccountBox enum.

---
+++ src/Enums/AccountBox.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MortgageUnion\Enums;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Class AccountBox
+ * @package MortgageUnion\Enums
+ *
+ * @method static AccountBox BOX1()
+ * @method static AccountBox BOX3()
+ *
+ */
+class AccountBox extends Enum
+{
+    const BOX1 = 1;
+    const BOX3 = 3;
+
+}
