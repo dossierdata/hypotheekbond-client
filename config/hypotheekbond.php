d683fe9714a44b204dd3b02c2bbfe0c93b7f9e03
Victor <victor.preda10@gmail.com>
2018-12-10 11:28:03 +0100

    Add default variables.

--- config/hypotheekbond.php
+++ config/hypotheekbond.php
@@ -1,4 +1,10 @@
 <?php
 
 return [
+    'advisorUser' => '',
+    'advisorPasword' => '',
+    'partnerUser' => 'Request at hypotheekbond',
+    'partnerPassword' => 'Request at hypotheekbond',
+    'uri' => 'https://www.hypotheekbond.nl/soapservices',
+    'wsdl' => 'https://www.hypotheekbond.nl/soapservices?wsdl'
 ];
