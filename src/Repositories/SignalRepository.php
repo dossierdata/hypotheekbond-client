af7caa9355a2715830d8304dd1b9c75bdf4e7e69
Victor <victor.preda10@gmail.com>
2019-01-15 13:51:08 +0100

    Add getByType method to the interface.

--- src/Repositories/SignalRepository.php
+++ src/Repositories/SignalRepository.php
@@ -3,7 +3,16 @@
 namespace MortgageUnion\Repositories;
 
 
+use Illuminate\Support\Collection;
+use MortgageUnion\Enums\SignalType;
+
 interface SignalRepository
 {
     public function all();
+
+    /**
+     * @param SignalType $type
+     * @return Collection
+     */
+    public function getByType(SignalType $type);
 }
